54ff98976b575fe6703b5e1009047bf2383cd0bb
adnan159 <osmanhaider159@gmail.com>
2022-08-28 00:00:49 +0600

    toggle added

--- multi-step-from.php
+++ multi-step-from.php
@@ -74,7 +74,7 @@
 
   <section>
     <div class="container">
-        <form>
+        <form id="rvx-export-form">
             <div class="step step-1 active">
                 <button type="button" class="next-btn">Export</button>
             </div>
@@ -82,17 +82,22 @@
                 <div class="form-group" style="display:flex; flex-direction: column;">
                     <label for="total-review">Total number of Review</label>
                     <input type="number" id="total-review" name="total-review" />
+
                     <label for="skip-no">Skip first n review</label>
                     <input type="number" id="skip-no" name="skip-review" />
+
                     <label for="date-from">Start Date</label>
                     <input type="date" id="date-from" name="date-from" />
+
                     <label for="date-to">End Date</label>
                     <input type="date" id="date-to" name="date-to" />
+
                     <label for="post-name">Post/Product</label>
                     <select id="post-name" name="post-name">
                       <option value="all">All</option>
                       <option value="album">Album</option>
                     </select>
+
                     <label for="selected-review">Select Review</label>
                     <select id="selected-review" name="selected-review">
                       <option value="1">1</option>
@@ -101,12 +106,14 @@
                       <option value="4">4</option>
                       <option value="5">5</option>
                     </select>
+
                     <label for="reviewer-type">Customer/Guest Review</label>
                     <select id="reviewer-type" name="reviewer-type">
                       <option value="all">All</option>
                       <option value="customer">Customer</option>
                       <option value="guest">Guest</option>
                     </select>
+
                     <label for="status">Status</label>
                     <select id="status" name="status">
                       <option value="all">All</option>
@@ -115,24 +122,33 @@
                       <option value="publish">Publish</option>
                       <option value="future">Future</option>
                     </select>
+
                     <label for="sort-columns">Sort Columns</label>
                     <select id="sort-columns" name="sort-columns">
                       <option value="commnet-id">Commnet ID</option>
                       <option value="comment-date">Comment Date</option>
                     </select>
+
                     <label for="sort-by">Sort By</label>
                     <select id="sort-by" name="sort-by">
                       <option value="ascending">Ascending</option>
                       <option value="decending">Descending</option>
                     </select>
+
                 </div>
                 <button type="button" class="previous-btn">Prev</button>
                 <button type="button" class="next-btn">Next</button>
             </div>
             <div class="step step-3">
                 <div class="form-group">
-                    <label for="country">country</label>
-                    <input type="text" id="country" name="country" />
+                    <input type="checkbox" name="select-all" id="rvx-select-all" onclick="toggle(this);" />
+                    <label for="select-all"> Column </label> <br> <br>
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                    <label for="vehicle1" name="vehicle1"> I have a bike</label><br>
+                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                    <label for="vehicle2"> I have a car</label><br>
+                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+                    <label for="vehicle3"> I have a boat</label>
                 </div>
                 <button type="button" class="previous-btn">Prev</button>
                 <button type="submit" class="submit-btn">Submit</button>
@@ -143,31 +159,35 @@
 
   <script>
 
-   const steps = Array.from(document.querySelectorAll("form .step"));  
-   const nextBtn = document.querySelectorAll("form .next-btn");  
-   const prevBtn = document.querySelectorAll("form .previous-btn");  
-   const form = document.querySelector("form");  
-   nextBtn.forEach((button) => {  
+  const steps = Array.from(document.querySelectorAll("form .step"));  
+  const nextBtn = document.querySelectorAll("form .next-btn");  
+  const prevBtn = document.querySelectorAll("form .previous-btn");  
+  const form = document.getElementById("rvx-export-form");  
+
+  nextBtn.forEach((button) => {  
     button.addEventListener("click", () => {  
      changeStep("next");  
     });  
-   });  
-   prevBtn.forEach((button) => {  
+  });  
+
+  prevBtn.forEach((button) => {  
     button.addEventListener("click", () => {  
      changeStep("prev");  
     });  
-   });  
-   form.addEventListener("submit", (e) => {  
+  });  
+
+  form.addEventListener("submit", (e) => {  
     e.preventDefault();  
     const inputs = [];  
-    form.querySelectorAll("input, select").forEach((input) => {  
+    form.querySelectorAll("input[type='number'],input[type='checkbox']:checked, select").forEach((input) => {  
      const { name, value } = input;  
      inputs.push({ name, value });  
     });  
     console.log(inputs);  
     form.reset();  
-   });  
-   function changeStep(btn) {  
+  });  
+
+  function changeStep(btn) {  
     let index = 0;  
     const active = document.querySelector(".active");  
     index = steps.indexOf(active);  
@@ -178,7 +198,15 @@
      index--;  
     }  
     steps[index].classList.add("active");  
-   }  
+  }  
+
+  function toggle(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+  }
 
 
   </script>
