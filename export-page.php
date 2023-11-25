e6d439aa76935a9a3b074ee8d5de400165cc73e8
Adnan <osmanhaider159@gmail.com>
2023-11-26 02:09:01 +0600

    tst git

--- export-page.php
+++ export-page.php
@@ -1,6 +1,10 @@
-
 <div class="body">
-	<div style="justify-content: center; display: flex;" > <h1>Export </h1> </div>
-	<div> <?php require_once 'multi-step-from.php'; ?> </div>
-<div>
-<div>test</div>
+    <div style="justify-content: center; display: flex;">
+        <h1>Export </h1>
+    </div>
+    <div> <?php require_once 'multi-step-from.php'; ?> </div>
+    <div>
+        <div>test</div>
+        <div>another test</div>
+    </div>
+</div>
